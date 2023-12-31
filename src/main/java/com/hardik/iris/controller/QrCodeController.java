33dc6d4c3c243b237d2cb3c15feff9b9550ce34a
Ahmed <ahmed.sayout18@gmail.com>
2023-12-31 17:28:48 +0100

    Qr code project commit 1

---
+++ src/main/java/com/hardik/iris/controller/QrCodeController.java
@@ -0,0 +1,55 @@
+package com.hardik.iris.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import com.hardik.iris.dto.QrCodeGenerationRequestDto;
+import com.hardik.iris.service.QrCodeService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class QrCodeController {
+
+	private final QrCodeService qrCodeService;
+
+	@PostMapping(value = "/generate")
+	@ResponseStatus(value = HttpStatus.OK)
+	@Operation(summary = "Returns a .png QR code with provided information decoded inside")
+	public void qrCodeGenerationHandler(
+			@Valid @RequestBody(required = true) final QrCodeGenerationRequestDto qrCodeGenerationRequestDto,
+			final HttpServletResponse httpServletResponse) throws IOException, WriterException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		qrCodeService.generate(qrCodeGenerationRequestDto, httpServletResponse);
+	}
+
+	@PutMapping(value = "/read", consumes = "multipart/form-data")
+	@ResponseStatus(value = HttpStatus.OK)
+	@Operation(summary = "returns decoded information inside provided QR code")
+	public ResponseEntity<?> read(
+			@Parameter(description = ".png image of QR code generated through this portal") @RequestParam(value = "file", required = true) MultipartFile file)
+			throws IOException, NotFoundException {
+		return qrCodeService.read(file);
+	}
+
+}
